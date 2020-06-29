26a5730125325562eb6837ba64a436c7860b51a0
kemokemo <t2wonderland@gmail.com>
2020-06-29 20:12:58 +0900

    feat: add version and ldflags for gox.

--- main.go
+++ main.go
@@ -18,10 +18,14 @@ var (
 	outerr  io.Writer = os.Stderr
 	exit              = os.Exit
 	cmdArgs []string  // commad line args hack
+
+	ver bool
 )
 
 func init() {
 	testing.Init() // require Go 1.13 or later
+	flag.BoolVar(&ver, "v", false, "display version")
+	flag.BoolVar(&ver, "version", false, "display version")
 	flag.Parse()
 	cmdArgs = flag.Args()
 }
@@ -31,6 +35,11 @@ func main() {
 }
 
 func run(args []string) int {
+	if ver {
+		fmt.Fprintf(out, "%s version %s.%s", Name, Version, revision)
+		return exitCodeOK
+	}
+
 	if len(args) == 0 {
 		fmt.Fprintln(outerr, "please specify args")
 		return exitCodeFailed
